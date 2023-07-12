bff47443d7eaea5b513aba31762eead071ed1330
Fabrice <mwanafunzifabrice@gmail.com>
2023-07-12 23:17:28 +0200

    push

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello Fabrice flutter'),
+      home: const MyHomePage(title: 'Hello I am Fabrice flutter'),
     );
   }
 }
